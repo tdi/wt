f80dba8db2232be6e1f4445aa1ede4db0732f96d
Darek Dwornikowski <dariusz.dwornikowski@gmail.com>
2026-06-22 16:08:47 +0200

    feat(list): wt ls command

--- src/list.rs
+++ src/list.rs
@@ -1,3 +1,26 @@
+use crate::git;
+
 pub fn run() -> anyhow::Result<()> {
-    todo!()
+    let worktrees = git::worktree_list(None)?;
+    let current = git::top_level(None).ok();
+
+    for wt in &worktrees {
+        let mark = if Some(&wt.path) == current.as_ref() {
+            "*"
+        } else {
+            " "
+        };
+        let branch_display = wt
+            .branch
+            .as_ref()
+            .map(|b| b.strip_prefix("refs/heads/").unwrap_or(b.as_str()))
+            .unwrap_or("(detached)");
+        println!(
+            "{} {}  {}",
+            mark,
+            wt.path.display(),
+            branch_display
+        );
+    }
+    Ok(())
 }
